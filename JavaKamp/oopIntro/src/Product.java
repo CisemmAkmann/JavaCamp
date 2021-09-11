5e3afa6135c7c9a2cb5408becae21759eb52ba32
CisemmAkmann <akmancisem45@gmail.com>
2021-09-11 16:35:45 +0300

    Update Product.java

--- JavaKamp/oopIntro/src/Product.java
+++ JavaKamp/oopIntro/src/Product.java
@@ -1,4 +1,18 @@
 public class Product {
+
+    //constructor,new yaptığında çalışır
+    public Product(){
+            System.out.println("Ben Çalıştım");
+    }
+    public Product(int id,String name,double unitPrice,String detail){
+        this(); // bu classın parametresiz olanını çalıştır.
+        this.id = id;
+        this.name= name;
+        this.unitPrice = unitPrice;
+        this.detail = detail;
+
+    }
+
     int id;
     String name;
     double unitPrice;
