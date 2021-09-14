e100842191d08ab31a4b22fcf48f8023122e3a0b
CisemmAkmann <akmancisem45@gmail.com>
2021-09-14 16:29:57 +0300

    .

--- JavaKamp/oopIntro/odev/src/CourseManager.java
+++
@@ -1,9 +0,0 @@
-public class CourseManager {
-    public void joinCourse(Courses courses){
-        System.out.println("Kursa kaydolundu " + courses.courseName);
-    }
-
-    public void completed(Courses courses){
-        System.out.println("Kurs tamamlandı " + courses.courseName + " Yüzdelik:" + courses.percent);
-    }
-}
