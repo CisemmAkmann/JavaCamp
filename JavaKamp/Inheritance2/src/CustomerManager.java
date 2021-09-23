1bce6a11d86cc3a10a4e2cf9aa44caf9b697d995
Çisem Akman <41484358+CisemmAkmann@users.noreply.github.com>
2021-09-23 16:19:50 +0300

    loglama

---
+++ JavaKamp/Inheritance2/src/CustomerManager.java
@@ -0,0 +1,8 @@
+public class CustomerManager {
+    public void add(Logger logger){
+        //müşteri ekleme kodları
+        System.out.println("Müşteri eklendi.");
+
+        logger.log();
+    }
+}
